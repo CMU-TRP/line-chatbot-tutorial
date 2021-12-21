81106a0120f3431991ff23682200e299fc8ea2c1
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-12-21 17:42:28 +0700

    initial commit

---
+++ src/main/java/cmu/se/cmulinechatbot/CmuLineChatbotApplication.java
@@ -0,0 +1,13 @@
+package cmu.se.cmulinechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmuLineChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmuLineChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cmu/se/cmulinechatbot/CmuLineChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package cmu.se.cmulinechatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmuLineChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
