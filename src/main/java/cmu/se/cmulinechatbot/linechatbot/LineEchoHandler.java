b8672f6df44ce5c365f17ddc60d5815e51b6f6ca
Chartchai Doungsaard <chartchai.d@cmu.ac.th>
2022-03-18 20:18:03 +0700

    add redis setting

--- src/main/java/cmu/se/cmulinechatbot/linechatbot/LineEchoHandler.java
+++ src/main/java/cmu/se/cmulinechatbot/linechatbot/LineEchoHandler.java
@@ -8,16 +8,41 @@ import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @LineMessageHandler
 @Slf4j
-public class LineEchoHandler {
+public class  LineEchoHandler {
+    int state = 1;
+    @Autowired
+    RedisTemplate<String,String> redisTemplate;
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-        System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
+        redisTemplate.opsForHash().put("x","y","z");
+        if (state == 1){
+            state = 2;
+            redisTemplate.opsForHash().put("LC",event.getSource().getUserId(),String.valueOf(state));
+            return TextMessage.builder().text(event.getMessage().getText()).build();
+        }else{
+            state = 1;
+
+            redisTemplate.opsForHash().put("LC",event.getSource().getUserId(),String.valueOf(state));
+            return TextMessage.builder().text("xx").build();
+        }
+
+//        if (data == null){
+//            data = "";
+//        }
+////        data = data + " " + event.getMessage().getText();
+////        System.out.println("event: " + event);
+//        switch (event.getMessage().getText()){
+//            case "1":
+//        }
+//        return new TextMessage(data);
     }
 
+    String data = "";
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
